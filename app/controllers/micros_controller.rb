80e25f95bcc2f4c4b9e60bd7dcefd14461e3cae5
PotHix <pothix@pothix.com>
2010-01-18 01:17:00 -0200

    Alterando para que funcione o edit de micros na mesma pagina de macros por ajax

--- app/controllers/micros_controller.rb
+++ app/controllers/micros_controller.rb
@@ -36,9 +36,14 @@ class MicrosController < ApplicationController
   # GET /micros/1/edit
   def edit
     @micro = Micro.find(params[:id])
-    #session[:micro] = @micro
-    #format.html { render :action => "/macros/#{@micro.macro_id}" }
-    render :partial=>"/micros/new_micro", :locals=>{:macro=>@micro.macro}
+    respond_to do |format|
+      format.html {render :partial=>"/micros/new_micro", :locals=>{:macro=>@micro.macro}}
+      format.js do
+        render :update do |page|
+          page.replace_html :micro, :partial => "edit_micro"
+        end
+      end
+    end
   end
 
   # POST /micros
@@ -50,24 +55,6 @@ class MicrosController < ApplicationController
     respond_to do |format|
       flash[:notice] = 'Micro was successfully created.'
       format.js
-#      if @micro.save
-#        flash[:notice] = %W'Micro was successfully created.'
-#        #format.html { redirect_to(@micro) }
-#        #format.xml  { render :xml => @micro, :status => :created, :location => @micro }
-#        format.js do
-#          render :update do |page|
-#            page.insert_html :bottom, 'tbl_micros', :partial=>'/micros/lista_micros', :locals=>{ :micros=>Micro.find(:all) }
-#            #page.visual_effect :highlight, "micro_#{@micro.id}"
-#          end
-#        end
-#      else
-#        #w.urubatan.com.br/um-exemplo-de-chat-com-ruby-on-rails-e-juggernaut-utilizando-ajax-push/
-#        #ttp://blog.maxaller.name/2008/03/simple-easy-ajax-form-validation-in-rails/
-#        format.html { render :action => "new" }
-#        format.xml  { render :xml => @micros.errors, :status => :unprocessable_entity }
-#
-#
-#      end
     end
   end
 
@@ -82,6 +69,11 @@ class MicrosController < ApplicationController
         #format.html { redirect_to(@micro) } ///Esse é o formato original
         format.html { redirect_to(:controller=>"macros", :action=>"show", :id=>"1") }
         format.xml  { head :ok }
+        format.js do
+          render :update do |page|
+            page.replace_html :"ordem_micro#{@micro.id}", @micro.ordem
+          end
+        end
       else
         format.html { render :action => "edit" }
         format.xml  { render :xml => @micro.errors, :status => :unprocessable_entity }
